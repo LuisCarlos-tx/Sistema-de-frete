2d2cbea4113635695cf29c80c5e8f535baa1d374
Luis Carlos <luiscarlostvx34@gmail.com>
2025-09-30 16:39:27 -0300

    ServicoDeEntrega.java

--- ServicoDeEntrega.java
+++ ServicoDeEntrega.java
@@ -3,24 +3,22 @@ import java.util.List;
 
 public class ServicoDeEntrega {
     private List<Fretavel> itensFretaveis;
+    
     public ServicoDeEntrega() {
         this.itensFretaveis = new ArrayList<>();
     }
     public void adicionarItem(Fretavel item) {
         itensFretaveis.add(item);
     }
-    public double calcularCustoTotal() {
-        double custoTotal = 0.0;
-        for (Fretavel item : itensFretaveis) {
-            custoTotal += item.calcularCustoFrete(0);
-        }
-        return custoTotal;
-    }
+    
     public double calcularCustoTotal(double distanciaEmKm) {
         double custoTotal = 0.0;
         for (Fretavel item : itensFretaveis) {
             custoTotal += item.calcularCustoFrete(distanciaEmKm);
         }
         return custoTotal;
+    }
+    public void limparItens() {
+        itensFretaveis.clear();
     }
 }
