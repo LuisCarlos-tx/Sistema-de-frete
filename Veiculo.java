de26e55a667cefa9267b44733422907d29071006
Luis Carlos <luiscarlostvx34@gmail.com>
2025-09-30 21:20:09 -0300

    Veiculo.java

--- Veiculo.java
+++ Veiculo.java
@@ -1,32 +1,21 @@
 public abstract class Veiculo implements Fretavel {
-    protected String placa;
-    protected int ano;
-    
+    private String placa;
+    private int ano;
+
     public Veiculo(String placa, int ano) {
         this.placa = placa;
         this.ano = ano;
     }
-    
-    public String getPlaca() { return placa; }
-    public int getAno() { return ano; }
-}
 
-public class Carro extends Veiculo {
-    public Carro(String placa, int ano) {
-        super(placa, ano);
+    public String getPlaca() {
+        return placa;
     }
-    
-    @Override
-    public double calcularCustoFrete(double distanciaEmKm) {
-        return distanciaEmKm * 1.25;
+
+    public int getAno() {
+        return ano;
     }
 }
 
-public class Motocicleta extends Veiculo {
-    public Motocicleta(String placa, int ano) {
-        super(placa, ano);
-    }
-    
     @Override
     public double calcularCustoFrete(double distanciaEmKm) {
         return distanciaEmKm * 0.45;
