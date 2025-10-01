cc20728bc24808836c459b83223a83f39a6bffc0
Luis Carlos <luiscarlostvx34@gmail.com>
2025-09-30 21:18:29 -0300

    Main.java

--- Main.java
+++ Main.java
@@ -1,52 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        
-        System.out.println("=== SISTEMA DE CÁLCULO DE FRETE ===\n");
-        
-        System.out.println("ENTREGA 1 - 10km");
-        ServicoDeEntrega entrega1 = new ServicoDeEntrega();
-        
-        Funcionario funcionario = new Funcionario("João Silva");
-        Carro carro = new Carro("ABC-1234", 2020);
-        
-        entrega1.adicionarItem(funcionario);
-        entrega1.adicionarItem(carro);
-    
-        double custoEntrega1 = entrega1.calcularCustoTotal(10.0);
-        
-        System.out.println("• Funcionário: " + funcionario.getNome());
-        System.out.println("• Veículo: Carro - Placa " + carro.getPlaca());
-        System.out.println("• Custo total: R$ " + String.format("%.2f", custoEntrega1));
 
-        System.out.println("\n  Detalhamento:");
-        System.out.println("  - Custo do funcionário: R$ " + String.format("%.2f", funcionario.calcularCustoFrete(10)));
-        System.out.println("  - Custo do carro: R$ " + String.format("%.2f", carro.calcularCustoFrete(10)));
-        
-        System.out.println("\nENTREGA 2 - 25km");
-        ServicoDeEntrega entrega2 = new ServicoDeEntrega();
-        
-        Autonomo autonomo = new Autonomo("Maria Santos");
-        Motocicleta moto = new Motocicleta("XYZ-5678", 2022);
-        
-        entrega2.adicionarItem(autonomo);
-        entrega2.adicionarItem(moto);
-        
-        double custoEntrega2 = entrega2.calcularCustoTotal(25.0);
-        
-        System.out.println("• Entregador: " + autonomo.getNome() + " (Autônomo)");
-        System.out.println("• Veículo: Motocicleta - Placa " + moto.getPlaca());
-        System.out.println("• Custo total: R$ " + String.format("%.2f", custoEntrega2));
-        
-        System.out.println("\n  Detalhamento:");
-        System.out.println("  - Custo do autônomo: R$ " + String.format("%.2f", autonomo.calcularCustoFrete(25)));
-        System.out.println("  - Custo da motocicleta: R$ " + String.format("%.2f", moto.calcularCustoFrete(25)));
-        
-        System.out.println("\n=== RESUMO ===");
-        System.out.println("Entrega 1 (10km): R$ " + String.format("%.2f", custoEntrega1));
-        System.out.println("Entrega 2 (25km): R$ " + String.format("%.2f", custoEntrega2));
-    
-        System.out.println("\n=== VERIFICAÇÃO DOS CÁLCULOS ===");
-        System.out.println("Entrega 1: R$ 5,00 (funcionário) + R$ 12,50 (carro) = R$ 17,50");
-        System.out.println("Entrega 2: R$ 23,75 (autônomo) + R$ 11,25 (moto) = R$ 35,00");
+        ServicoDeEntrega entrega1 = new ServicoDeEntrega(10);
+        entrega1.adicionarItem(new Carro("ABC-1234", 2020));
+        entrega1.adicionarItem(new Funcionario());
+        System.out.println("Custo total da entrega 1: R$ " + entrega1.calcularCustoTotal());
+
+        ServicoDeEntrega entrega2 = new ServicoDeEntrega(25);
+        entrega2.adicionarItem(new Motocicleta("XYZ-9876", 2022));
+        entrega2.adicionarItem(new Autonomo());
+        System.out.println("Custo total da entrega 2: R$ " + entrega2.calcularCustoTotal());
     }
 }
