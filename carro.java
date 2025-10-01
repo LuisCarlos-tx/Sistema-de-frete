a54272b6c3825c3ede89467d5792d7f9ffef7afe
Luis Carlos <luiscarlostvx34@gmail.com>
2025-09-30 21:12:11 -0300

    Carro.java

---
+++ carro.java
@@ -0,0 +1,10 @@
+public class Carro extends Veiculo {
+    public Carro(String placa, int ano) {
+        super(placa, ano);
+    }
+
+    @Override
+    public double calcularCustoFrete(double distanciaEmKm) {
+        return distanciaEmKm * 1.25;
+    }
+}
