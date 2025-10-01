71d3a628579c3f1a99e22008cfdd911752847aef
Luis Carlos <luiscarlostvx34@gmail.com>
2025-09-30 21:12:57 -0300

    Motocicleta.java

---
+++ Motocicleta.java
@@ -0,0 +1,10 @@
+public class Motocicleta extends Veiculo {
+    public Motocicleta(String placa, int ano) {
+        super(placa, ano);
+    }
+
+    @Override
+    public double calcularCustoFrete(double distanciaEmKm) {
+        return distanciaEmKm * 0.45;
+    }
+}
