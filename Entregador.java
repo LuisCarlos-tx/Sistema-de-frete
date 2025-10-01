432164fdec3629eb0bba6e1b28b4d6065871c4d4
Luis Carlos <luiscarlostvx34@gmail.com>
2025-09-30 21:15:39 -0300

    Entregador.java

--- Entregador.java
+++ Entregador.java
@@ -1,25 +1,3 @@
 public abstract class Entregador implements Fretavel {
-    protected String nome;
-    public Entregador(String nome) {
-        this.nome = nome;
-    }
-    public String getNome() { return nome; }
 }
-public class Funcionario extends Entregador {
-    public Funcionario(String nome) {
-        super(nome);
-    }
-    @Override
-    public double calcularCustoFrete(double distanciaEmKm) {
-        return 5.00;
-    }
-}
-public class Autonomo extends Entregador {
-    public Autonomo(String nome) {
-        super(nome);
-    }
-    @Override
-    public double calcularCustoFrete(double distanciaEmKm) {
-        return distanciaEmKm * 0.95;
-    }
-}
+
